9c873fce0a7d55440fe312b250d05172d69fffa4
me <me@example.com>
2026-05-06 01:23:56 -0500

    Add support for non-packaged locally-installed scrapers.
    Rather than always checking the CommunityScrapers list for a matching scraper, it will now first check what's installed (regardless of where it's installed from), falling back to looking for a community scraper to install.
    
    I was working on adding a new site to an existing scraper, so when I was testing the update, scraperSearch would return early because the new site's URL is not in the CommunityScrapers list (yet).

--- src/scraper-index.ts
+++ src/scraper-index.ts
@@ -1,10 +1,11 @@
 // import types from scraper index
 import { scraperExport } from "../types/scraperIndex.js"
+import { localScraper } from "../types/stashapp.js"
 import axios from "axios"
 import { StashApp } from "./stash-app.js"
 
 // scraper index searcher and installer
-async function searchScrapers(url: string): Promise<string[]> {
+async function searchCommunityScrapers(url: string): Promise<string[]> {
   // fetch communityScrapers
   const communityScrapers = await axios.get("https://stashapp.github.io/CommunityScrapers/assets/scrapers.json")
     .then(res => res.data as scraperExport[])
@@ -16,19 +17,39 @@ async function searchScrapers(url: string): Promise<string[]> {
   return matchedScrapers.map(scraper => scraper.filename.replace('../scrapers/', '').replace('.yml', '').split("/")[0])
 }
 
+// Checks scrapers locally installed on the Stash instance for one which handles `url`. Returns the IDs of any matching scrapers.
+async function searchLocalScrapers(url: string, stash: StashApp): Promise<string[]> {
+  const localScrapers = await stash.getLocalScrapers()
+  const matchedScrapers = localScrapers.filter((scraper: localScraper) =>
+    scraper.sites && scraper.sites.some(pattern => url.includes(pattern))
+  )
+  return matchedScrapers.map(scraper => scraper.id)
+}
+
 // handle url scrapersearch
 export async function scraperSearch(url: string, stash: StashApp): Promise< { error: string } | { success: string, id: string } > {
-  // search in CommunityScrapers
-  const matchedScrapers = await searchScrapers(url)
-  // if no results, return empty array
-  if (matchedScrapers.length === 0) return { "error": "No scrapers found for the provided URL." }
-  // check for existing scrapers
-  const existingScrapers = await stash.getExistingScrapers()
-  // check against IDs
-  const hasExistingScrapers = matchedScrapers.filter(scraper => existingScrapers.includes(scraper))
-  // if no existing and only one matched, install it
-  if (hasExistingScrapers.length === 0 && matchedScrapers.length === 1) {
-    const scraperId = matchedScrapers[0]
+  // check whether there is already a scraper installed for this URL
+  const matchedLocalScrapers = await searchLocalScrapers(url, stash)
+  // if one existing, return success
+  if (matchedLocalScrapers.length == 1) {
+    return {
+      success: `Scraper ${matchedLocalScrapers[0]} already installed.`,
+      id: matchedLocalScrapers[0]
+    }
+  } else if (matchedLocalScrapers.length > 1) {
+    return {
+      success: "Multiple scrapers already installed",
+      id: matchedLocalScrapers[1]
+    }
+  }
+
+  // else, search CommunityScrapers
+  const matchedCommunityScrapers = await searchCommunityScrapers(url)
+  // if still no results, return empty array
+  if (matchedCommunityScrapers.length === 0) return { "error": "No scrapers found for the provided URL." }
+  // if only one matched, install it
+  if (matchedCommunityScrapers.length === 1) {
+    const scraperId = matchedCommunityScrapers[0]
     console.log(`Installing scraper: ${scraperId}`)
     return stash.installPackage(scraperId)
       .then((jobId: number) => stash.awaitJobFinished(jobId))
@@ -36,19 +57,8 @@ export async function scraperSearch(url: string, stash: StashApp): Promise< { er
         success: `Scraper ${scraperId} installed successfully.`,
         id: scraperId
       }))
-  } else if (matchedScrapers.length > 1 && hasExistingScrapers.length === 0) {
+  } else {
     // if multiple, don't install
     return { "error": "Multiple scrapers found for the provided URL. Cowardly refusing to install." }
-  } else if (hasExistingScrapers.length == 1) {
-    // if one existing, return success
-    return {
-      success: `Scraper ${hasExistingScrapers[0]} already installed.`,
-      id: hasExistingScrapers[0]
-    }
-  } else {
-    return {
-      success: "Multiple scrapers already installed",
-      id: hasExistingScrapers[1]
-    }
   }
 }
